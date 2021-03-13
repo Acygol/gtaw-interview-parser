5b8da95a6cbd9847c9e59d8f36de308fc6b52ad1
Acygol <35696477+Acygol@users.noreply.github.com>
2021-03-13 11:08:59 +0100

    Updated broken header image link

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ const TPLTVAR_QUESTION_ANSWER_CONTENT = '$QUESTION_ANSWER_CONTENT$'; // The answ
 // The main template is the parent template; individual question templates are parsed
 // and then embedded within this template.
 const MAIN_TEMPLATE = `[divbox=white]
-[center][img]https://www.upload.ee/image/12808254/recruitment.png[/img][/center] 
+[center][img]https://i.imgur.com/VTE8HAs.png[/img][/center] 
 
 [divbox=#284066][color=white][center][size=150][font=AmerType Md BT]INTERVIEW - ${TPLTVAR_INTERVIEWEE_NAME}[/font][/size][/center][/color][/divbox]
 
