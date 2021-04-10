b527e258f58c48b659f3ad4d6b70149aca02c3f6
Acygol <jefferson.on.yt@gmail.com>
2021-04-10 05:16:31 +0200

    Reworked HTML form and added customization options

--- js/main.js
+++ js/main.js
@@ -1,15 +1,42 @@
 /*
+//  MIT License
 //
-//  Written and developed by Acygol (JeffersonKay).
+//  Copyright (c) 2021 Acygol
 //
-//  Initially used for the Training and Recruitment division of the 
-//  GTAW Fire Department, repurposed for the GTAW State Fire Marshals.
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
 //
-//  Edit, copy, do whatever. I don't care.
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+//
+//  By JeffersonKay - please ask before sharing this repository. I like to keep record
+//  of roughly how many people this tool has reached.
+//
+//  Initially used for the Training and Recruitment division of the GTAW Fire Department, 
+//  repurposed for the GTAW State Fire Marshals. I tried making it as generalized as possible.
+//
+//  Edit, copy, do whatever. I don't care. Just leave the credits; read the license.
+//
+//  TODO: this file needs some cleaning up, comments, etc. I'll eventually get to it.
 //
 */
 
 // Template variables
+const TPLTVAR_HEADER_IMAGE_LINK       = '$HEADER_IMAGE_LINK$';       // The header image link provided by the user
+const TPLTVAR_DIVBOX_COLOR            = '$DIVBOX_COLOR$';            // The color for the title divbox and the legend headers
 const TPLTVAR_INTERVIEWEE_NAME        = '$INTERVIEWEE_NAME$';        // The name of the interviewee
 const TPLTVAR_PREINTERVIEW_TEXT       = '$PREINTERVIEW_TEXT$';       // The preinterview text - any dialog/RP before the first question
 const TPLTVAR_QUESTIONS_CONTAINER     = '$QUESTIONS_CONTAINER$';     // Replaced by a string containing all the question templates combined
@@ -19,19 +46,19 @@ const TPLTVAR_QUESTION_ANSWER_CONTENT = '$QUESTION_ANSWER_CONTENT$'; // The answ
 // The main template is the parent template; individual question templates are parsed
 // and then embedded within this template.
 const MAIN_TEMPLATE = `[divbox=white]
-[center][img]https://i.imgur.com/VTE8HAs.png[/img][/center] 
+[center]${TPLTVAR_HEADER_IMAGE_LINK}[/center] 
 
-[divbox=#284066][color=white][center][size=150][font=AmerType Md BT]INTERVIEW - ${TPLTVAR_INTERVIEWEE_NAME}[/font][/size][/center][/color][/divbox]
+[divbox=${TPLTVAR_DIVBOX_COLOR}][color=white][center][size=150][font=AmerType Md BT]INTERVIEW - ${TPLTVAR_INTERVIEWEE_NAME}[/font][/size][/center][/color][/divbox]
 
 [justify]
-[legend=#284066, Preinterview]${TPLTVAR_PREINTERVIEW_TEXT}[/legend]
+[legend=${TPLTVAR_DIVBOX_COLOR}, Preinterview]${TPLTVAR_PREINTERVIEW_TEXT}[/legend]
 ${TPLTVAR_QUESTIONS_CONTAINER}
 [/justify]
 [/divbox]
 
 Response format:
 [code]
-[legend=#284066, Evaluation]
+[legend=${TPLTVAR_DIVBOX_COLOR}, Evaluation]
 [b]Reviewer's name:[/b]
 [b]Reviewer's badge number:[/b]
 [b]Evaluation:[/b]
@@ -41,16 +68,27 @@ Response format:
 `
 
 // Individual question template
-const QUESTION_TEMPLATE = `[legend=#284066, ${TPLTVAR_QUESTION_TEXT}]${TPLTVAR_QUESTION_ANSWER_CONTENT}[/legend]`
+const QUESTION_TEMPLATE = `[legend=${TPLTVAR_DIVBOX_COLOR}, ${TPLTVAR_QUESTION_TEXT}]${TPLTVAR_QUESTION_ANSWER_CONTENT}[/legend]`
+
+// Errors
+let g_HasError = false;
+const g_ErrorDiv = document.getElementById('div-error');
+
+// Default colors
+const g_DefaultDivboxColor  = '#ffffff'; // White
+const g_DefaultRoleplayColor = '#AD82CE'; // Some shade of purple
 
 // Form data
 let g_InterviewerName = ''; // Interviewer name
 let g_IntervieweeName = ''; // Interviewee name
 let g_BadgeNumber = '';     // Interviewer's badge number
+let g_HeaderImageLink = ''; // Header image link
 let g_Participants = [];    // Interview participants to include
+let g_DivboxColor = '';     // Divbox color
+let g_RoleplayColor = '';   // Role play color
 
 // Handle the file selector element...
-const fileSelector = document.getElementById('logsFileInput');
+const fileSelector = document.getElementById('input-chatlog-file');
 fileSelector.addEventListener('change', (event) => {
     const file = event.target.files[0];
 
@@ -61,35 +99,69 @@ fileSelector.addEventListener('change', (event) => {
 
     const reader = new FileReader();
     reader.onload = function(event) {
-        g_InterviewerName   = document.getElementById('interviewerNameInput').value;
-        g_IntervieweeName   = document.getElementById('intervieweeNameInput').value;
-        g_BadgeNumber       = document.getElementById('badgeNumberInput').value;
+        g_InterviewerName   = document.getElementById('input-interviewer-name').value;
+        g_IntervieweeName   = document.getElementById('input-interviewee-name').value;
+        g_BadgeNumber       = document.getElementById('input-interviewer-badge').value;
+        g_HeaderImageLink   = parseImgurLink();
+        g_DivboxColor       = parseColor('input-divbox-color', g_DefaultDivboxColor);
+        g_RoleplayColor     = parseColor('input-roleplay-color', g_DefaultRoleplayColor);
         g_Participants      = parseParticipants();
 
-        g_Participants.push(g_InterviewerName); // The interviewer is a participant
-        g_Participants.push(g_IntervieweeName); // The interviewee is a participant
-
-        logToInterview(event.target.result);
+        // Only parse when there weren't any custom errors.
+        if (!g_HasError) {
+            parseChatLog(event.target.result.split('\n'));
+        }
+        g_HasError = false; // Don't force the user to refresh after correcting an error.
     };
 
     reader.readAsText(file);
 });
 
-function logToInterview(text) {
-    const rankSelectionElem = document.getElementById('rankSelection');
-    const rankSelectionId = rankSelectionElem.options.selectedIndex;
-    const rankName = rankSelectionElem.options[rankSelectionId].text; // Interviewer's rank
-
-    parseChatLog(text.split('\n'));
-}
-
 function parseParticipants() {
     let participants = [];
-    let participantsText = document.getElementById('otherParticipantsInput').value;
+    let participantsText = document.getElementById('input-participants-names').value;
     for (let i = 0; i < participantsText.split('\n').length; i++) {
         participants.push(participantsText[i]);
     }
+    participants.push(g_InterviewerName); // The interviewer is a participant.
+    participants.push(g_IntervieweeName); // The interviewee is a participant.
     return participants;
+}
+
+function parseImgurLink() {
+    let headerLink = document.getElementById('input-header-img-link').value;
+    if (headerLink)
+    {
+        // When the header link isn't conforming to the format, let the user know.
+        if (!(headerLink.endsWith('.png') || headerLink.endsWith('.jpeg'))) {
+            showError("The provided Imgur link isn't valid. Double check that it has .jpeg or .png at the end.");
+            return;
+        }
+
+        headerLink = `[img]${headerLink}[/img]`;
+    }
+    return headerLink;
+}
+
+function parseColor(elementId, defaultValue = "") {
+    let element = document.getElementById(elementId);
+    if (element == null) {
+        console.log("This shouldn't happen... Unless someone's been toying with the source code. Anyway, parseColor argument elementId is null.");
+        return;
+    }
+
+    let color = element.value;
+
+    // Assign the default value if the user hasn't entered a color code.
+    if (!color)
+        color = defaultValue;
+
+    // Append the '#' necessary for BB code to recognize the color if the user forgot
+    // to include it. No biggie, I will add it for you, dear user.
+    if (!color.startsWith('#')) {
+        color = '#' + color;
+    }
+    return color;
 }
 
 /*
@@ -154,7 +226,7 @@ function parseChatLog(lines) {
         }
 
         // Skip duplicate /ame lines...
-        if (i > 0 && isInterviewerAme(lines[i]) && isInterviewerAme(lines[i+1])) {
+        if (i < lines.length && isInterviewerAme(lines[i]) && isInterviewerAme(lines[i+1])) {
             console.log(`line skipped (duplicate ame): ${lines[i]}`);
             continue;
         }
@@ -188,7 +260,7 @@ function parseChatLog(lines) {
 
         // When the line is a roleplay emote, apply the color...
         if (isRoleplayLine(lines[i])) {
-            lines[i] = `[color=#AD82CE]${lines[i]}[/color]`;
+            lines[i] = `[color=${g_RoleplayColor}]${lines[i]}[/color]`;
         }
 
         // The line is a question...
@@ -213,10 +285,18 @@ function parseChatLog(lines) {
 
     // Output a formatted topic
     let formattedTemplate = MAIN_TEMPLATE;
+    formattedTemplate = formattedTemplate.replace(TPLTVAR_HEADER_IMAGE_LINK, g_HeaderImageLink);
     formattedTemplate = formattedTemplate.replace(TPLTVAR_PREINTERVIEW_TEXT, preinterviewContent);
     formattedTemplate = formattedTemplate.replace(TPLTVAR_INTERVIEWEE_NAME, g_IntervieweeName);
     formattedTemplate = formattedTemplate.replace(TPLTVAR_QUESTIONS_CONTAINER, allQuestions);
-    logsContent.value = formattedTemplate;
+
+    const divColorRegex = new RegExp(escapeRegExp(TPLTVAR_DIVBOX_COLOR), 'g');
+    formattedTemplate = formattedTemplate.replace(divColorRegex, g_DivboxColor);
+    document.getElementById('output-box').value = formattedTemplate;
+}
+
+function escapeRegExp(unescapedString) {
+    return unescapedString.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 }
 
 // formatQuestion...
@@ -224,6 +304,7 @@ function parseChatLog(lines) {
 //      q_content:  the answer and all the dialog as a response to the question
 function formatQuestion(q_header, q_content) {
     let formattedTemplate = QUESTION_TEMPLATE;
+    formattedTemplate = formattedTemplate.replace(TPLTVAR_DIVBOX_COLOR, g_DivboxColor);
     formattedTemplate = formattedTemplate.replace(TPLTVAR_QUESTION_TEXT, q_header);
     formattedTemplate = formattedTemplate.replace(TPLTVAR_QUESTION_ANSWER_CONTENT, q_content);
     return formattedTemplate;
@@ -277,4 +358,13 @@ function isRoleplayLine(line, check_meta = false) {
 
 function isInterviewerAme(line) {
     return line.includes(`> ${g_InterviewerName}`);
+}
+
+function showError(error) {
+    g_ErrorDiv.className = "alert alert-danger my-4";
+    g_ErrorDiv.innerHTML = `${error}`;
+    g_HasError = true;
+
+    // Force the user back to the top. They may not see the error otherwise.
+    g_ErrorDiv.scrollIntoView();
 }
